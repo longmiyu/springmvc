b0a7abdee1d86bfbd1ba86f48a3607eb154c0585
longmiyu <923679940@qq.com>
2018-05-14 15:10:02 +0800

    修改

--- src/main/java/com/springdemo/controller/DemoController.java
+++ src/main/java/com/springdemo/controller/DemoController.java
@@ -3,18 +3,32 @@ package com.springdemo.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.springdemo.entity.user;
+import com.springdemo.service.DemoService;
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
+	@Resource
+	private DemoService ds;
 
-    @RequestMapping("/index")
+    public DemoService getDs() {
+		return ds;
+	}
+
+	public void setDs(DemoService ds) {
+		this.ds = ds;
+	}
+
+	@RequestMapping("/index")
     public String index(){
         return "login";
     }
@@ -26,18 +40,16 @@ public class DemoController {
     	
 		return "demo";
     	
-    };
-    
+    };   
     @RequestMapping("/login1")
    @ResponseBody
     public Map<String,Object> login1 (user u) {
     	System.out.println("name:"+u.getName());
     	System.out.println("password:"+u.getPassword());
     	Map<String,Object> mp = new HashMap<String,Object>();
-    	mp.put("name", u.getName());
-    	mp.put("password", u.getPassword());
-    	
-		return mp;
-    	
+//    	mp.put("name", u.getName());
+//    	mp.put("password", u.getPassword());   
+    	mp = ds.queryUser(u.getName());
+		return mp;  	
     };
 }
